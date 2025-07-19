ba3d7c550b62857f69fbf2c7fdd97ae6fd732efb
Planet X <socialplanetxlive@gmail.com>
2025-07-19 22:35:31 +0530

    Update uri.ts

--- lib/uri.ts
+++ lib/uri.ts
@@ -1,4 +1,4 @@
-export const backendUrl = "http://192.168.1.100:3000/api"
+export const backendUrl = "http://localhost:3000/api"
 export const AWS_ACCESS_KEY_ID = 'Key'
 export const AWS_SECRET_ACCESS_KEY = 'Key'
 export const AWS_REGION = 'key'
